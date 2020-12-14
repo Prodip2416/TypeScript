72d7a95870534d297ed4d134b8863d4809c67931
prodip2416 <prodip.sarker.cse@gmail.com>
2020-12-15 00:13:36 +0600

    union types in ts

---
+++ Basic/union-types.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+// let data:number|string|boolean = 100;
+var data = 100;
+data = 'Prodip';
+data = true;
+console.log(data);
---
+++ Basic/union-types.ts
@@ -0,0 +1,8 @@
+export{}
+
+// let data:number|string|boolean = 100;
+let data:any = 100;
+data = 'Prodip';
+data = true;
+
+console.log(data);
