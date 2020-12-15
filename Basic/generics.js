053b0b421e995b39f3591f4a72e7080f2920967c
prodip2416 <prodip.sarker.cse@gmail.com>
2020-12-15 13:42:31 +0600

    generics in ts

---
+++ Basic/generics.js
@@ -0,0 +1,6 @@
+function getData(data) {
+    return data;
+}
+console.log('data');
+console.log(true);
+console.log({ name: 'prodip', age: 25 });
---
+++ Basic/generics.ts
@@ -0,0 +1,7 @@
+function getData<T>(data: T): T {
+    return data;
+}
+
+console.log('data');
+console.log(true);
+console.log({ name: 'prodip', age: 25 });
